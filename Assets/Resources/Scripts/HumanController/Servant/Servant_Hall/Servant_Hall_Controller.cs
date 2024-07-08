35cf2d5ca7c7ff59017f7a7aa2730d82a265a254
wlsdkrla <wlswlsdk23@naver.com>
2024-07-08 18:17:31 +0900

    [rename] rename file

---
+++ Assets/Resources/Scripts/HumanController/Servant/Servant_Hall/Servant_Hall_Controller.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Servant_Hall_Controller : ServantController
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+
+    }
+    public override void Move()
+    {
+
+    }
+}
